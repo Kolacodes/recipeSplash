c88d7956b1162daa9ed1fa06202b2a04471509c5
Bello Kolawole Mustapha <50749200+Kolacodes@users.noreply.github.com>
2021-03-19 03:59:30 +0100

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var express = require ("express"),
     port = process.env.PORT || 5000,
-    nodemailer = require("nodemailer"),
-    methodOverride = require("method-override"),
-    cookieParser        = require("cookie-parser"),
-    flash      = require("connect-flash"),
     bodyParser = require("body-parser");
 
 app = express();
@@ -11,7 +7,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-// AIzaSyCbf0RRCKYNPXFFTfZvh8Z11ZgHDFp_dyE
 
 app.get("/", function(req, res) {
     
